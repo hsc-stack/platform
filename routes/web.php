98a687e2198e3aecffbc3a934f7f99fa691bad61
Tajim <com.tajim@gmail.com>
2026-06-13 16:51:59 +0600

    add admin panel dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\NodeController;
 use App\Http\Controllers\ResourceController;
 use App\Http\Controllers\SubjectController;
@@ -9,6 +10,9 @@ Route::inertia('/privacy-policy', 'legal/PrivacyPolicy');
 Route::inertia('/terms-service', 'legal/TermsConditions');
 Route::inertia('/join', 'platform/JoinTeam');
 Route::inertia('/about-us', 'platform/AboutUs');
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [DashboardController::class, 'index']);
+});
 
 Route::get('/', [SubjectController::class, 'index']);
 Route::get('/resources/{resource:id}', [ResourceController::class, 'show']);
