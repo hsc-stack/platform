cf6dd5503d0b63f0f6665057d5d6ebea8350acb2
Tajim <com.tajim@gmail.com>
2026-06-15 17:22:03 +0600

    implement throttle middleware to prevent abuse

--- routes/web.php
+++ routes/web.php
@@ -12,61 +12,70 @@
     use App\Http\Controllers\SubjectController;
     use Illuminate\Support\Facades\Route;
 
-    Route::inertia('/privacy-policy', 'legal/PrivacyPolicy');
-    Route::inertia('/terms-service', 'legal/TermsConditions');
-    Route::inertia('/join', 'platform/JoinTeam');
 
-    Route::prefix('admin')->middleware(['auth', 'verified', 'role:manager|admin|editor'])->name('admin.')->group(function () {
+    Route::middleware('throttle:30,1')->group(function () {
 
-        Route::get('/', [DashboardController::class, 'index']);
-        Route::get('/subjects', [AdminSubjectController::class, 'index'])->name("subjects.index");
-        Route::get('/subjects/create', [AdminSubjectController::class, 'create'])->name("subjects.create");
-        Route::get('/subjects/edit/{subject}', [AdminSubjectController::class, 'edit'])->name("subjects.edit");
 
-        Route::get('/subjects/{subject:slug}/nodes/create', [AdminNodeController::class, 'create'])->name('nodes.create');
-        Route::get('/nodes/edit/{node}', [AdminNodeController::class, 'edit'])->name('nodes.edit');
 
-        Route::get('/resources/create', [AdminResourceController::class, 'create']);
-        Route::get('/resources/edit/{resource}', [AdminResourceController::class, 'edit']);
+        Route::prefix('admin')->middleware(['auth', 'verified', 'role:manager|admin|editor'])->name('admin.')->group(function () {
 
-        Route::get('/users', [AdminUserController::class, 'index'])->name("users.index");
-        Route::get('/users/create', [AdminUserController::class, 'create']);
-        Route::get('/users/edit/{user}', [AdminUserController::class, 'edit']);
+            Route::get('/', [DashboardController::class, 'index']);
+            Route::get('/subjects', [AdminSubjectController::class, 'index'])->name("subjects.index");
+            Route::get('/subjects/create', [AdminSubjectController::class, 'create'])->name("subjects.create");
+            Route::get('/subjects/edit/{subject}', [AdminSubjectController::class, 'edit'])->name("subjects.edit");
 
-        Route::get('/subjects/{subject:slug}/nodes/{path?}', [AdminNodeController::class, 'show'])->name('nodes.index')->where('path', '.*');
+            Route::get('/subjects/{subject:slug}/nodes/create', [AdminNodeController::class, 'create'])->name('nodes.create');
+            Route::get('/nodes/edit/{node}', [AdminNodeController::class, 'edit'])->name('nodes.edit');
+
+            Route::get('/resources/create', [AdminResourceController::class, 'create']);
+            Route::get('/resources/edit/{resource}', [AdminResourceController::class, 'edit']);
+
+            Route::get('/users', [AdminUserController::class, 'index'])->name("users.index");
+            Route::get('/users/create', [AdminUserController::class, 'create']);
+            Route::get('/users/edit/{user}', [AdminUserController::class, 'edit']);
+
+            Route::get('/subjects/{subject:slug}/nodes/{path?}', [AdminNodeController::class, 'show'])->name('nodes.index')->where('path', '.*');
+        });
+
+        Route::prefix('admin')->middleware(['auth', 'verified', 'role:editor|admin'])->name('admin.')->group(function () {
+
+            Route::patch('/subjects/edit/{subject}', [AdminSubjectController::class, 'update'])->name("subjects.update");
+            Route::post('/subjects', [AdminSubjectController::class, 'store'])->name("subjects.store");
+
+            Route::post('/subjects/{subject}/nodes', [AdminNodeController::class, 'store'])->name('nodes.store');
+            Route::patch('/subjects/{subject}/nodes/{node}', [AdminNodeController::class, 'update'])->name('nodes.patch');
+
+            Route::post('/resources', [AdminResourceController::class, 'store']);
+            Route::post('/resources/{resource}/patch', [AdminResourceController::class, 'update']);
+        });
+
+        Route::prefix('admin')->middleware(['auth', 'verified', 'role:admin'])->name('admin.')->group(function () {
+            Route::delete('/resources/{resource}', [AdminResourceController::class, 'destroy']);
+            Route::delete('/subjects/{subject}', [AdminSubjectController::class, 'destroy']);
+            Route::delete('/nodes/{node}', [AdminNodeController::class, 'destroy']);
+            Route::delete('/users/{user}', [AdminUserController::class, 'destroy']);
+
+            Route::post('/users', [AdminUserController::class, 'store'])->name("admin.users.store");
+            Route::patch('/users/{user}', [AdminUserController::class, 'update'])->name("admin.users.update");
+        });
     });
 
-    Route::prefix('admin')->middleware(['auth', 'verified', 'role:editor|admin'])->name('admin.')->group(function () {
+    Route::post('/login', [AuthController::class, 'login'])
+        ->middleware('throttle:5,1');
 
-        Route::patch('/subjects/edit/{subject}', [AdminSubjectController::class, 'update'])->name("subjects.update");
-        Route::post('/subjects', [AdminSubjectController::class, 'store'])->name("subjects.store");
 
-        Route::post('/subjects/{subject}/nodes', [AdminNodeController::class, 'store'])->name('nodes.store');
-        Route::patch('/subjects/{subject}/nodes/{node}', [AdminNodeController::class, 'update'])->name('nodes.patch');
+    Route::middleware('throttle:60,1')->group(function () {
+        Route::inertia('/privacy-policy', 'legal/PrivacyPolicy');
+        Route::inertia('/terms-service', 'legal/TermsConditions');
+        Route::inertia('/join', 'platform/JoinTeam');
 
-        Route::post('/resources', [AdminResourceController::class, 'store']);
-        Route::post('/resources/{resource}/patch', [AdminResourceController::class, 'update']);
+        Route::get('/about-us', [AboutUsController::class, 'index']);
+
+        Route::get('/login', [AuthController::class, 'index'])->name('login');
+        Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+        Route::get('/', [SubjectController::class, 'index'])->name('index');
+        Route::get('/resources/{resource:id}', [ResourceController::class, 'show']);
+        Route::get('/{subject:slug}', [SubjectController::class, 'show']);
+        Route::get('/{subject:slug}/{path}', [NodeController::class, 'show'])->where('path', '.*');
     });
-
-    Route::prefix('admin')->middleware(['auth', 'verified', 'role:admin'])->name('admin.')->group(function () {
-        Route::delete('/resources/{resource}', [AdminResourceController::class, 'destroy']);
-        Route::delete('/subjects/{subject}', [AdminSubjectController::class, 'destroy']);
-        Route::delete('/nodes/{node}', [AdminNodeController::class, 'destroy']);
-        Route::delete('/users/{user}', [AdminUserController::class, 'destroy']);
-
-        Route::post('/users', [AdminUserController::class, 'store'])->name("admin.users.store");
-        Route::patch('/users/{user}', [AdminUserController::class, 'update'])->name("admin.users.update");
-    });
-
-
-    Route::get('/about-us', [AboutUsController::class, 'index']);
-
-    Route::get('/login', [AuthController::class, 'index'])->name('login');
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-
-    Route::get('/', [SubjectController::class, 'index'])->name('index');
-    Route::get('/resources/{resource:id}', [ResourceController::class, 'show']);
-    Route::get('/{subject:slug}', [SubjectController::class, 'show']);
-    Route::get('/{subject:slug}/{path}', [NodeController::class, 'show'])->where('path', '.*');
