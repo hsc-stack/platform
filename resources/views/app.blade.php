d610ea8e4781e31cec1a7bb181325c00588618c6
Tajim <com.tajim@gmail.com>
2026-06-18 20:44:48 +0600

    add custom logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,15 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        {{-- <link rel="icon" href="/favicon.ico" sizes="any"> --}}
-        {{-- <link rel="icon" href="/favicon.svg" type="image/svg+xml"> --}}
-        {{-- <link rel="apple-touch-icon" href="/apple-touch-icon.png"> --}}
+        <link rel="icon" href="/favicon.ico" sizes="any">
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         @fonts
 
         @vite(['resources/css/app.css', 'resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
         <x-inertia::head>
-            <title>{{ config('app.name', 'Laravel') }}</title>
+            <title>{{ config('app.name', 'HSC Stack') }}</title>
         </x-inertia::head>
     </head>
     <body class="font-sans antialiased">
