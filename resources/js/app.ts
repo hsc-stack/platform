6d676351564ce18618a8c8e3f477279cabb83d8b
Tajim <com.tajim@gmail.com>
2026-07-11 10:31:31 +0600

    undo custom error handler of inertia

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,21 +1,12 @@
-import { createInertiaApp, router } from '@inertiajs/vue3';
+import { createInertiaApp } from '@inertiajs/vue3';
 import AdminLayout from './layouts/AdminLayout.vue';
 import AppLayout from './layouts/AppLayout.vue';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-router.on('invalid', (event) => {
-    const response = event.detail.response;
-
-    if (response?.status === 403 && response.headers.get('cf-mitigated')) {
-        event.preventDefault();
-        window.location.reload();
-    }
-});
-
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
-    layout: (name)=>{
+    layout: (name) => {
         switch (true) {
             case name.startsWith('admin/'):
                 return AdminLayout;
